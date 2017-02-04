faad111a6ec12d030dd470d222f4de894a77173c
Scott Woodend <woodendscott@gmail.com>
2017-02-04 14:48:56 -0500

    add function to insert url pairings into db

---
+++ db/mongo.js
@@ -0,0 +1,33 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const urlSchema = mongoose.Schema({
+  long_url: {type: String, required: true, unique: true},
+  short_url: {type: String, required: true, unique: true}
+});
+
+var Url = mongoose.model('Url', urlSchema);
+
+
+
+function addUrlPairing(longUrl, shortUrl){
+  let pairing = new Url({
+    long_url: longUrl,
+    short_url: shortUrl
+  });
+
+  mongoose.connect('mongodb://localhost/url_shortener', function(err){
+  if (err) throw err;
+    pairing.save(function(err){
+      if (err) throw err;
+      console.log('pairing saved to db url_shortener, collection urls');
+      mongoose.disconnect();
+    });
+  });
+}
+
+module.exports = {
+  UrlModel : Url,
+  addUrlPairing : addUrlPairing
+};
