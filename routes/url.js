fdf0ad415f6a2483725c52ef9f9237129faa15d9
Scott Woodend <woodendscott@gmail.com>
2017-02-04 15:35:05 -0500

    save short/long pairing in db and send a response to user

--- routes/url.js
+++ routes/url.js
@@ -1,11 +1,19 @@
 'use strict';
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
+const getRandomString = require('../helpers/string.js');
+const mongo = require('../db/mongo.js');
+const Url = require('../db/mongo.js').UrlModel;
 
 
 router.get('/:longUrl*', function(req, res, next) {
   let longUrl = req.params.longUrl + req.params[0];
-  res.send(longUrl);
+  let shortUrl = getRandomString();
+
+  mongo.addUrlPairing(longUrl, shortUrl, function(){
+    res.send({long_url:longUrl, short_url: shortUrl});
+  });
+
 });
 
 module.exports = router;
