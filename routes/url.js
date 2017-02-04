fea52ce14331902d1d0a66ef4927f594f7c27241
Scott Woodend <woodendscott@gmail.com>
2017-02-04 15:50:15 -0500

    use validUrl to verify the url received

--- routes/url.js
+++ routes/url.js
@@ -4,15 +4,22 @@ const router = express.Router();
 const getRandomString = require('../helpers/string.js');
 const mongo = require('../db/mongo.js');
 const Url = require('../db/mongo.js').UrlModel;
+const validUrl = require('valid-url');
+console.log(validUrl);
 
 
 router.get('/:longUrl*', function(req, res, next) {
   let longUrl = req.params.longUrl + req.params[0];
   let shortUrl = getRandomString();
 
-  mongo.addUrlPairing(longUrl, shortUrl, function(){
-    res.send({long_url:longUrl, short_url: shortUrl});
-  });
+  if (validUrl.isHttpUri(longUrl) || validUrl.isHttpsUri(longUrl)){
+    mongo.addUrlPairing(longUrl, shortUrl, function(){
+      res.send({long_url:longUrl, short_url: shortUrl});
+    });
+  } else {
+    res.send({'Error' : "Invalid URL"});
+  }
+
 
 });
 
